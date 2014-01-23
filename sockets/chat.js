7da255bdad615c2c464172b1ab854fb3b9f52747
renanmoreira <renan.skz@gmail.com>
2014-01-23 18:34:07 -0300

    Colocando controle de acesso nas salas de chat

--- sockets/chat.js
+++ sockets/chat.js
@@ -1,15 +1,41 @@
 module.exports = function (io) {
-	var sockets = io.sockets;
+	var crypto = require('crypto'),
+		md5 = crypto.createHash('md5'),
+		sockets = io.sockets;
 
 	sockets.on('connection', function(client) {
 		var session = client.handshake.session,
 			usuario = session.usuario;
 
+		client.on('join', function(sala) {
+			if (sala)
+				sala = sala.replace('?', '');
+			else {
+				var timestamp = new Date().toString();
+				var md5 = crypto.createHash('md5');
+
+				sala = md5.update(timestamp).digest('hex');
+			}
+
+			client.set('sala', sala);
+			client.join(sala);
+		});
+
+		client.on('disconnect', function(client) {
+			client.get('sala', function(erro, sala) {
+				client.leave(sala);
+			});
+		});
+
 		client.on('send-server', function(msg) {
 			var mensagemFormatada = "<b>" + usuario.nome + ":</b>" + msg + "<br/>";
 
-			client.emit('send-client', mensagemFormatada);
-			client.broadcast.emit('send-client', mensagemFormatada);	
+			client.get('sala', function(erro, sala) {
+				var data = { email: usuario.email, sala: sala };
+
+				client.broadcast.emit('new-message', data);
+				sockets.in(sala).emit('send-client', mensagemFormatada);
+			});
 		});
 	});
 }
